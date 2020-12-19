35c600fa39f7a72522293e0692aed08512ed88af
ganirockz <ganeshrapeti8@gmail.com>
2020-12-19 15:55:43 +0530

    [Ganesh]Added: home page of employee payroll

--- src/components/payroll-form/payroll-form.jsx
+++ src/components/payroll-form/payroll-form.jsx
@@ -115,6 +115,7 @@ const PayrollForm = (props) => {
 
         employeeService.addEmployee(object).then(data => {
             console.log("data added successfully");
+            props.history.push("/home");
         }).catch(err => {
             console.log("error after Add data");
         })
